1a2c8aeb75a2f73642f68e78263548118376bdae
Mario Rovito <alessandro.r14@gmail.com>
2022-03-22 16:26:41 +0100

    add array zucchinaNew

--- script.js
+++ script.js
@@ -77,8 +77,62 @@ console.log(zucchina);
 //Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
 
+//Crea 10 oggetti che rappresentano una zucchina.
 
+let zucchinaNew = [
 
+    {
+        varietà : 1,
+        peso: 1,
+        lunghezza: 1,
+    },
+    {
+        varietà : 1,
+        peso: 2,
+        lunghezza: 16,
+    },
+    {
+        varietà : 1,
+        peso: 3,
+        lunghezza: 3,
+    },
+    {
+        varietà : 1,
+        peso: 4,
+        lunghezza: 17,
+    },
+    {
+        varietà : 1,
+        peso: 5,
+        lunghezza: 5,
+    },
+    {
+        varietà : 1,
+        peso: 6,
+        lunghezza: 18,
+    },
+    {
+        varietà : 1,
+        peso: 7,
+        lunghezza: 7,
+    },
+    {
+        varietà : 1,
+        peso: 8,
+        lunghezza: 19,
+    },
+    {
+        varietà : 1,
+        peso: 9,
+        lunghezza: 9,
+    },
+    {
+        varietà : 1,
+        peso: 10,
+        lunghezza: 20,
+    }
+
+];
 
 
 
