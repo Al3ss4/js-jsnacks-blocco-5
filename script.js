61f978e6da908c808a44420de0c12a4f030bc3a1
Mario Rovito <alessandro.r14@gmail.com>
2022-03-22 15:22:27 +0100

    add html and js code

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log('JS OK!');
+
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+
+
+
+
+
+
+
+
+//Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+
+
+
+
+
+
+
+
+
+
+
+//Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
