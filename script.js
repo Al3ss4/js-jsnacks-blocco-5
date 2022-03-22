48521c4c0a84ac7e2f2ade554bb08197eb97aae7
Mario Rovito <alessandro.r14@gmail.com>
2022-03-22 15:34:07 +0100

    add array zucchina

--- script.js
+++ script.js
@@ -3,16 +3,69 @@ console.log('JS OK!');
 // Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.
 
 
+//creo un array di 10 oggetti che rappresentano una zucchina
 
+let zucchina = [
 
+    {
+        varietà : 1,
+        peso: 1,
+        lunghezza: 1,
+    },
+    {
+        varietà : 1,
+        peso: 2,
+        lunghezza: 2,
+    },
+    {
+        varietà : 1,
+        peso: 3,
+        lunghezza: 3,
+    },
+    {
+        varietà : 1,
+        peso: 4,
+        lunghezza: 4,
+    },
+    {
+        varietà : 1,
+        peso: 5,
+        lunghezza: 5,
+    },
+    {
+        varietà : 1,
+        peso: 6,
+        lunghezza: 6,
+    },
+    {
+        varietà : 1,
+        peso: 7,
+        lunghezza: 7,
+    },
+    {
+        varietà : 1,
+        peso: 8,
+        lunghezza: 8,
+    },
+    {
+        varietà : 1,
+        peso: 9,
+        lunghezza: 9,
+    },
+    {
+        varietà : 1,
+        peso: 10,
+        lunghezza: 10,
+    }
 
+];
+
+console.log(zucchina);
 
 
 
 
 //Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
-
-
 
 
 
